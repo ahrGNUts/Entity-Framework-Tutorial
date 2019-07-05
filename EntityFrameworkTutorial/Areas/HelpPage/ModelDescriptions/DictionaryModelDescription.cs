fe50a7bb49b2e2cc45c70697a495d2e34315b87e
Patrick <revmydev@gmail.com>
2019-07-04 19:28:11 -0700

    Add project files.

---
+++ EntityFrameworkTutorial/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EntityFrameworkTutorial.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ EntityFrameworkTutorial/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EntityFrameworkTutorial.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
